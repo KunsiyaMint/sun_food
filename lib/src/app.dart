3112bae1b0f9fc52dceb94b5e63cfe6f8245210c
mint <kunsiyamintmint@gmail.com>
2019-03-03 14:11:16 +0700

    get json success

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' show get;
+import 'dart:convert';
+import 'package:flutter/cupertino.dart';
+import './models/json_model.dart';
 
 class App extends StatefulWidget {
   @override
@@ -6,10 +10,19 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
-      int idInt = 0;
+  int idInt = 0;
+  myClickFloating() async {
+    print('You click Floating');
+    idInt += 1;
+    print('idInt = $idInt');
+    var response = await get(
+        "http://androidthai.in.th/sun/getFoodWhereIdMint.php?isAdd=true&id=$idInt");
+    var converted = json.decode(response.body);
+    print('converted ==> $converted');
+  }
+
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
       title: 'Sun Food',
       home: Scaffold(
@@ -20,11 +33,7 @@ class _AppState extends State<App> {
         floatingActionButton: FloatingActionButton(
           child: Icon(Icons.add_circle),
           onPressed: () {
-            setState(() {
-              print('You click Floating');
-              idInt += 1;
-              print('idInt = $idInt');
-            });
+            myClickFloating();
           },
         ),
         body: Text('idInt = $idInt'),
